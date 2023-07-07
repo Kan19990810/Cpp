461a53811d66ea792e953913fbfe5457fba138db
Kan19990810 <xkk19990810@gmail.com>
2023-07-07 15:24:25 +0800

    acwing code

---
+++ cmake-build-debug/CMakeFiles/CppPractice.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/File/CppPractice/ACWing/3752.cpp" "CMakeFiles/CppPractice.dir/ACWing/3752.cpp.obj" "gcc" "CMakeFiles/CppPractice.dir/ACWing/3752.cpp.obj.d"
+  "D:/File/CppPractice/ACWing/4443.cpp" "CMakeFiles/CppPractice.dir/ACWing/4443.cpp.obj" "gcc" "CMakeFiles/CppPractice.dir/ACWing/4443.cpp.obj.d"
+  "D:/File/CppPractice/ACWing/4672.cpp" "CMakeFiles/CppPractice.dir/ACWing/4672.cpp.obj" "gcc" "CMakeFiles/CppPractice.dir/ACWing/4672.cpp.obj.d"
+  "D:/File/CppPractice/main.cpp" "CMakeFiles/CppPractice.dir/main.cpp.obj" "gcc" "CMakeFiles/CppPractice.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CppPractice.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CppPractice.
+# This may be replaced when dependencies are built.

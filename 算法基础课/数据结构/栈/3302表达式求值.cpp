4b95e9cf27ba4bce9eb3ed4a63ea7d3a91597b40
Kan19990810 <xkk19990810@gmail.com>
2023-09-01 08:34:26 +0800

    1

--- 算法基础课/数据结构/栈/3302表达式求值.cpp
+++ 算法基础课/数据结构/栈/3302表达式求值.cpp
@@ -7,11 +7,25 @@
 using namespace std;
 
 stack<int> num;
-stack<int> op;
+stack<char> op;
+
+void eval()
+{
+    int x;
+    auto b = num.top(); num.pop();
+    auto a = num.top(); num.pop();
+    auto c = op.top(); op.pop();
+    if (c == '+') x = a + b;
+    else if (c == '-') x = a - b;
+    else if (c == '*') x = a * b;
+    else x = a / b;
+    num.push(x);
+}
+
 
 int main()
 {
-    unordered_map<char, int> pr{{"+", 1}, {"-", 1}, {"*", 2}, {"/", 2}};
+    unordered_map<char, int> pr{{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}};
     string str;
     cin >> str;
     for (int i = 0; i < str.size(); i ++ )
@@ -25,15 +39,21 @@ int main()
                 x = x * 10 + str[j ++] - '0';
             }
             num.push(x);
+            i  = j - 1;
         }
         else if (c == '(') op.push(c);
         else if (c == ')')
         {
             while (op.top() != '(') eval();
+            op.pop();
         }
         else
         {
             while (op.size() && pr[op.top()] >= pr[c]) eval();
+            op.push(c);
         }
     }
+    while(op.size()) eval();
+    cout << num.top() << endl;
+    return 0;
 }

670d460ce5e5c75b953da209bbebf399ee9f1e4e
lap'WSL <xk19990810@163.com>
2024-04-02 09:12:08 +0800

    read

---
+++ dayly/4/allPossibleFBT.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ *
+ * 给你一个整数 n ，请你找出所有可能含 n 个节点的 真二叉树 ，并以列表形式返回。答案中每棵树的每个节点都必须符合 Node.val == 0 。
+
+答案的每个元素都是一棵真二叉树的根节点。你可以按 任意顺序 返回最终的真二叉树列表。
+
+真二叉树 是一类二叉树，树中每个节点恰好有 0 或 2 个子节点。
+ */
+class Solution
+{
+public:
+    vector<TreeNode *> allPossibleFBT(int n)
+    {
+    }
+};

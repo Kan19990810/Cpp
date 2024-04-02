9e443f83cbfe219993a921c5b1386fcbcfc2ce33
lap'WSL <xk19990810@163.com>
2024-04-02 12:42:35 +0800

    4.2

--- dayly/4/allPossibleFBT.cpp
+++ dayly/4/allPossibleFBT.cpp
@@ -8,17 +8,32 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- *
- * 给你一个整数 n ，请你找出所有可能含 n 个节点的 真二叉树 ，并以列表形式返回。答案中每棵树的每个节点都必须符合 Node.val == 0 。
-
-答案的每个元素都是一棵真二叉树的根节点。你可以按 任意顺序 返回最终的真二叉树列表。
-
-真二叉树 是一类二叉树，树中每个节点恰好有 0 或 2 个子节点。
  */
 class Solution
 {
 public:
     vector<TreeNode *> allPossibleFBT(int n)
     {
+        if (n % 2 == 0)
+        {
+            return {};
+        }
+        vector<vector<TreeNode *>> dp(n + 1);
+        dp[1] = {new TreeNode(0)};
+        for (int i = 3; i <= n; i += 2)
+        {
+            for (int j = 1; j < i; j += 2)
+            {
+                for (TreeNode *leftTree : dp[j])
+                {
+                    for (TreeNode *rightTree : dp[i - j - 1])
+                    {
+                        TreeNode *root = new TreeNode(0, leftTree, rightTree);
+                        dp[i].push_back(root);
+                    }
+                }
+            }
+        }
+        return dp[n];
     }
 };

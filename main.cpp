a50fbbddb2788b5d8edb99b648f0a6d7cca3ede1
lap'WSL <xk19990810@163.com>
2024-05-19 09:14:35 +0800

    push

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include <bits/stdc++.h>
+#include <thread>
 using namespace std;
+
+int *ptr;
+
+void func1()
+{
+    cout << "func1 pre ptr:" << ptr << endl;
+    int b = 2;
+    ptr = &b;
+    cout << "func1 cur ptr:" << ptr << endl;
+}
+
+void func2()
+{
+    cout << "func2 pre ptr:" << *ptr << endl;
+    *ptr = 3;
+    cout << "func2 cur ptr:" << *ptr << endl;
+}
 
 int main()
 {
-    int x = std::ceil(1.0 / 2.0);
-    std::cout << typeid(x).name() << std::endl;
-
-    return 0;
+    int a = 1;
+    ptr = &a;
+    thread t1(func1);
+    thread t2(func2);
+    t1.join();
+    t2.join();
 }
